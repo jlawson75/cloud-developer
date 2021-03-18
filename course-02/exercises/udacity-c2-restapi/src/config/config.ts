88e90ce4a38391637d3c7f1b3b5cc21adfd1a312
jlawson75 <jerry@eicalifornia.com>
2021-03-18 11:22:35 -0600

    updated sequelize credentials

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramlawsondev",
+    "password": "udagramlawsondev",
+    "database": "udagramlawsondev",
+    "host": "udagramlawsondev.cqpuyull1uls.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
