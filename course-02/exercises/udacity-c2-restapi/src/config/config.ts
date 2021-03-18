aa91f3cc7a5b27c6215e3ab42155dce39e317db8
jlawson75 <jerry@eicalifornia.com>
2021-03-18 13:50:12 -0600

    added credentials for aws database

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -5,9 +5,9 @@ export const config = {
     "database": "udagramlawsondev",
     "host": "udagramlawsondev.cqpuyull1uls.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-lawson-dev"
   },
   "prod": {
     "username": "",
